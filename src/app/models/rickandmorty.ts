b44a23bdb0fc4ed341024ca7a10434f69a6cf05a
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2024-02-02 13:45:46 -0600

    creating rickandmortymodel

--- src/app/models/rickandmorty.ts
+++ src/app/models/rickandmorty.ts
@@ -1,2 +1,22 @@
 export interface Rickandmorty {
+
+    "id": number,
+  "name": string,
+  "status": string,
+  "species": string,
+  "type": string,
+  "gender": string,
+  "origin": {
+    "name": string,
+    "url": string
+  },
+  "location": {
+    "name": string,
+    "url": string
+  },
+  "image": string,
+  "episode": string[],
+  "url": string,
+  "created": string
 }
+
